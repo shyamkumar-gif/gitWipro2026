7cd4bf14af537a3080e371e8ff55078f2ca4effd
shyamkumar <shyamkumarushkalwar@gmail.com>
2026-01-13 13:58:51 +0530

    added day 1 .py files

---
+++ TC_sets.py
@@ -0,0 +1,20 @@
+myset={1,2,3,4,5,6,4,6,7,7,8,9,10}
+print(myset)
+
+for x in myset:
+    print(x)
+myset.add(11)
+print(myset)
+
+A={1,2,3,4,5}
+B={10,9,8,7,6}
+
+print(A|B)
+print(A&B)
+
+print (2 in A)
+
+set1=frozenset
+var = {12, 13, 14, 15}  #becomes immutable
+print(set1)
+
