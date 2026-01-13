c5f8a88d38519ca152b1207d7815297aba5f0c99
shyamkumar <shyamkumarushkalwar@gmail.com>
2026-01-13 15:12:27 +0530

    Remove files

--- TC_ArithmenticOperations.py
+++
@@ -1,8 +0,0 @@
-a=100
-b=30
-print("Addition",a+b)
-print("Subraction",a-b)
-print("Multiplication",a*b)
-print("Division",a/b)
-print("Modules",a%b)
-print("Floor division",a//b)
--- TC_BuiltinFunctions.py
+++
@@ -1,6 +0,0 @@
-import math
-
-print(math.sqrt(9))
-print(math.log(100))
-print(math.gamma(100))
-
